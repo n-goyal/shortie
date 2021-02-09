8ae55d5752dda6c3c340a5cf098ffefe5575e6b4
Nitin Goyal <goyalnitin634@gmail.com>
2021-02-09 16:26:52 +0530

    🌴 moved model

--- models/model.js
+++ models/model.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-// const { nanoid } = require("nanoid");
 
 const urlSchema = new mongoose.Schema({
 	urlCode: String,
 	longUrl: String,
 	shortUrl: String,
 	date: {
-		type: string,
+		type: String,
 		default: Date.now,
 	},
 });
